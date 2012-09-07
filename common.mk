44adc0d48721ba3d53e1004c76c0ebfc7c8e22fc
Hashcode <hashcode0f@gmail.com>
2012-09-07 11:22:12 -0700

    common setup for motorola devices on ICS

---
+++ common.mk
@@ -0,0 +1,139 @@
+#
+# This is the product configuration for a full common
+#
+include $(all-subdir-makefiles)
+
+COMMON_FOLDER := device/motorola/common
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+## (3)  Finally, the least specific parts, i.e. the non-GSM-specific aspects
+
+# Device overlay
+DEVICE_PACKAGE_OVERLAYS += $(COMMON_FOLDER)/overlay
+
+# high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+PRODUCT_PACKAGES := \
+    charger \
+    charger_res_images
+
+# Audio handled by device
+
+# Hardware HALs
+PRODUCT_PACKAGES += \
+    camera.omap4 \
+    libinvensense_mpl
+
+PRODUCT_PACKAGES += \
+    libaudioutils \
+    libaudiohw_legacy
+
+# BlueZ test tools
+PRODUCT_PACKAGES += \
+    hciconfig \
+    hcitool
+
+# Modem
+PRODUCT_PACKAGES += \
+    libaudiomodemgeneric \
+    libreference-cdma-sms \
+    rild \
+    radiooptions
+
+# Wifi
+PRODUCT_PACKAGES += \
+    lib_driver_cmd_wl12xx \
+    dhcpcd.conf \
+    hostapd.conf \
+    wifical.sh \
+    wpa_supplicant.conf \
+    TQS_D_1.7.ini \
+    crda \
+    regulatory.bin \
+    calibrator
+
+# Wifi Direct
+PRODUCT_PACKAGES += \
+    ti_wfd_libs
+
+# Bluetooth
+PRODUCT_PACKAGES += \
+    bt_sco_app \
+    uim-sysfs 
+
+# Release utilities
+PRODUCT_PACKAGES += \
+    common_releaseutils-check_kernel \
+    common_releaseutils-finalize_release \
+    common_releaseutils-mke2fs \
+    common_releaseutils-tune2fs
+
+PRODUCT_PACKAGES += \
+    evtest \
+    sensor.test \
+    camera_test \
+    Superuser \
+    su \
+    DockAudio \
+    safestrapmenu
+
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory \
+    FileManager \
+    MusicFX \
+    Apollo
+
+# Permissions files
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:/system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.camera.front.xml:/system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/base/data/etc/android.hardware.camera.xml:/system/etc/permissions/android.hardware.camera.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:/system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:/system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/base/data/etc/android.hardware.sensor.compass.xml:/system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:/system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:/system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.telephony.cdma.xml:/system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:/system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:/system/etc/permissions/android.hardware.touchscreen.multitouch.distinct.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.xml:/system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:/system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.wifi.direct.xml:/system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/base/data/etc/handheld_core_hardware.xml:/system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/base/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
+
+# Prebuilts
+PRODUCT_COPY_FILES += \
+    $(COMMON_FOLDER)/prebuilt/bin/strace:system/bin/strace
+
+# Phone settings
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# Kexec files
+ifneq ($(TARGET_DEVICE),solana)
+PRODUCT_COPY_FILES += \
+    $(COMMON_FOLDER)/kexec/arm_kexec.ko:system/etc/kexec/arm_kexec.ko \
+    $(COMMON_FOLDER)/kexec/atags:system/etc/kexec/atags \
+    $(COMMON_FOLDER)/kexec/kexec:system/etc/kexec/kexec \
+    $(COMMON_FOLDER)/kexec/kexec.ko:system/etc/kexec/kexec.ko \
+    $(COMMON_FOLDER)/kexec/uart.ko:system/etc/kexec/uart.ko
+endif
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# still need to set english for audio init
+PRODUCT_LOCALES += en_US
+
+# stuff specific to ti OMAP4 hardware
+$(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
+$(call inherit-product, hardware/ti/wpan/ti-wpan-products.mk)
+$(call inherit-product-if-exists, vendor/motorola/common/proprietary/imgtec/sgx-imgtec-bins.mk)
+$(call inherit-product-if-exists, device/ti/proprietary-open/wl12xx/wlan/wl12xx-wlan-fw-products.mk)
+
